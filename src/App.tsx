a3b1b1537b6f77a8ee8ab9030e534a1283981487
Mandeep Malik <mandeepmalik2003@gmail.com>
2024-08-19 06:27:11 +0530

    added solana wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import MnemonicGenerate from './components/Mnemonic_generator'
+import SolanaWallet from './components/SolanaWalet'
 
 
 function App() {
   return (
     <>
       <MnemonicGenerate />
+      <SolanaWallet mnemonic = "" />
     </>
+
   )
 }
 
---
+++ src/components/SolanaWalet.tsx
@@ -0,0 +1,32 @@
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { mnemonicToSeedSync } from "bip39";
+import { derivePath } from "ed25519-hd-key";
+import { useState } from "react";
+import nacl from "tweetnacl";
+
+
+
+export default function SolanaWallet( { mnemonic }: { mnemonic: string } ) {
+    const [publicKeys,setPublicKeys] = useState<PublicKey[]>([]);
+    const [currIndex,setCurrIndex] = useState(0);
+
+    function createKeys(){
+        const seed = mnemonicToSeedSync(mnemonic);
+        const path = `m/44'/501'/${currIndex}'/0'`;
+        const derivedSeed = derivePath(path,seed.toString("hex")).key;
+        const secret = nacl.sign.keyPair.fromSeed(derivedSeed);
+        const privateKey = secret.secretKey;
+        const keypair = Keypair.fromSecretKey(privateKey);
+
+        const publickey = keypair.publicKey;
+
+        setPublicKeys([...publicKeys, publickey]);
+        setCurrIndex(currIndex+1);
+    }
+    return(
+        <div>
+            <button onClick={createKeys}>Add Solana Wallet</button>
+            {publicKeys.map(p=> <div>{p.toString()}</div>)}
+        </div>
+    )
+}

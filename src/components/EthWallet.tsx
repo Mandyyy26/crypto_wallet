0e44a17cda477f03950cb76d7e8b0fbe73ce4699
Mandeep Malik <mandeepmalik2003@gmail.com>
2024-08-20 15:39:35 +0530

    private key added

--- src/components/EthWallet.tsx
+++ src/components/EthWallet.tsx
@@ -3,9 +3,11 @@ import { mnemonicToSeedSync } from "bip39";
 import { useState } from "react";
 import { HDNodeWallet } from "ethers";
 import { Wallet } from "ethers";
+import { set } from "mongoose";
 
 export default function EthWallet({mnemonic}: {mnemonic: string}) {
     const [publicKeys,setPublicKeys] = useState<string[]>([]);
+    const [privateKeys,setPrivateKeys] = useState<string[]>([]);
     const [currIndex,setCurrIndex] = useState(0);
 
     function createKeys(){
@@ -16,6 +18,7 @@ export default function EthWallet({mnemonic}: {mnemonic: string}) {
         const privateKey = child.privateKey;
         const wallet = new Wallet(privateKey);
 
+        setPrivateKeys([...privateKeys, wallet.privateKey]);
         setPublicKeys([...publicKeys, wallet.address]);
         setCurrIndex(currIndex+1);
 
@@ -23,7 +26,10 @@ export default function EthWallet({mnemonic}: {mnemonic: string}) {
     return(
         <div>
         <button className="bg-black text-white rounded-full m-4 p-4 hover:bg-slate-800" onClick={createKeys}>Add Eherum Wallet</button>
-        <div>{publicKeys.map(p=> <li className="font-mono">Eth - {p.toString()}</li>)}</div>
+        <div className="grid grid-cols-2 gap-1">
+                <div>{publicKeys.map(p=> <li className="font-mono"><b className="text-purple-700">Public key  <br /></b> ETH- {p.toString()}</li>)}</div>
+                <div>{privateKeys.map(p=> <li className="font-mono"><b className="text-green-600">Private key <br /></b>{p.toString()}</li>)}</div>
+            </div>
         </div>
     )
 }
